bbddba49f04e469a7485b0bbac82fb74409dd900
Eran Cohen <eranco@redhat.com>
2021-04-07 13:29:00 +0300

    Updated assisted-service-onprem.sh so that all assisted-service data persist to file

--- assisted-service/assisted-service-onprem.sh
+++ assisted-service/assisted-service-onprem.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+install -d -m 0777  /etc/assisted-service/postgress-data/
+
 echo "Starting assisted-installer onprem"
 podman  pod rm assisted-installer-onprem -f | true
 podman pod create --name assisted-installer-onprem -p 5432:5432,8000:8000,8090:8090,8080:8080
-podman run -dt --pod assisted-installer-onprem --env-file /etc/assisted-service/onprem-environment --name db quay.io/ocpmetal/postgresql-12-centos7
+podman run -dt --pod assisted-installer-onprem -v /etc/assisted-service/postgress-data:/var/lib/pgsql/data:rw,z --env-file /etc/assisted-service/onprem-environment --name db quay.io/ocpmetal/postgresql-12-centos7
 podman run -dt --pod assisted-installer-onprem --env-file /etc/assisted-service/onprem-environment -v /etc/assisted-service/nginx.conf:/opt/bitnami/nginx/conf/server_blocks/nginx.conf:z --name ui quay.io/ocpmetal/ocp-metal-ui:latest
 echo "Wait for the DB to start..."
 sleep 15
