e7b5a97de5ad9976689cc93371a949acb3bfac26
Eran Cohen <eranco@redhat.com>
2021-04-07 21:03:27 +0300

    Run assisted-installer on the node post reboot

--- assisted-service/assisted-service-onprem.sh
+++ assisted-service/assisted-service-onprem.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-
+# Unsure this is still required
 install -d -m 0777  /etc/assisted-service/postgress-data/
 
 echo "Starting assisted-installer onprem"
 podman  pod rm assisted-installer-onprem -f | true
 podman pod create --name assisted-installer-onprem -p 5432:5432,8000:8000,8090:8090,8080:8080
-podman run -dt --pod assisted-installer-onprem -v /etc/assisted-service/postgress-data:/var/lib/pgsql/data:rw,z --env-file /etc/assisted-service/onprem-environment --name db quay.io/ocpmetal/postgresql-12-centos7
+podman run -dt --pod assisted-installer-onprem -v /etc/assisted-service/postgress-data:/opt/app-root/src:rw,z --env-file /etc/assisted-service/onprem-environment --name db quay.io/ocpmetal/postgresql-12-centos7
 podman run -dt --pod assisted-installer-onprem --env-file /etc/assisted-service/onprem-environment -v /etc/assisted-service/nginx.conf:/opt/bitnami/nginx/conf/server_blocks/nginx.conf:z --name ui quay.io/ocpmetal/ocp-metal-ui:latest
 echo "Wait for the DB to start..."
 sleep 15
+if [ -f /etc/assisted-service/postgress-data/installer.sql ]; then
+  echo "Restoring postgress data"
+  podman exec -it db psql installer <  /etc/assisted-service/postgress-data/installer.sql
+  podman exec -it db bash -c "psql installer <  /opt/app-root/src/installer.sql"
+fi
+
 podman run --pod assisted-installer-onprem --env-file /etc/assisted-service/onprem-environment --env DUMMY_IGNITION=False \
- -v /etc/assisted-service/fake-livecd.iso:/data/livecd.iso:z -v /bin/coreos-installer:/data/coreos-installer:z --restart always \
+ -v /etc/assisted-service/assisted-data:/data:z -v /bin/coreos-installer:/data/coreos-installer:z --restart always \
  --name installer quay.io/eranco74/bm-inventory:onprem
+
---
+++ assisted-service/override-ignition.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+until [ -f /usr/local/bin/bootstrap-in-place.sh ]; do
+  echo "Waiting for bootstrap-in-place.sh"
+  sleep 5
+done
+
+# echo this code to the end of the file
+
+cat <<EOT >> /usr/local/bin/bootstrap-in-place.sh
+echo "Create postgress backup"
+podman exec -it db pg_dump installer >  /etc/assisted-service/postgress-data/installer.sql
+
+# copy the original master ignition
+cp /opt/openshift/master.ign /etc/assisted-service/
+# Generate the ignition
+echo "Rendering ignition"
+podman run --rm -v /usr/local/bin:/data/bin -v /etc/assisted-service/:/data quay.io/coreos/fcct:release --pretty -d /data --strict /data/assisted-service.fcc > /opt/openshift/master.ign
+EOT
+
+## Get the host id:
+#CLUSTER_ID=c1dca253-bb31-4e41-982a-482885d10e1a
+#curl -s http://127.0.0.1:8090/api/assisted-install/v1/clusters/$CLUSTER_ID | jq ".hosts[0].id"
+#
+## update the ignition // add config
+#
+## patch the host ignition
+#curl -X PATCH  -H "Content-Type: application/json" -d @/etc/assisted-service/assisted-service.ign http://127.0.0.1:8090/api/assisted-install/v1/clusters/$CLUSTER_ID/hosts/$HOST_ID/ignition
